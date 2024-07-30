ed809b130952b422f9e422c89fb66131081cdf48
wjhssb <948816373@qq.com>
2024-07-30 22:28:44 +0800

    add definition of cynapse.

--- include/neuron_jh/neuron.hpp
+++ include/neuron_jh/neuron.hpp
@@ -1,12 +1,29 @@
 #include<vector>
 
+using value_t = float;
+
+template<typename TActFn, typename TVal = value_t>
 class Cynapse;
 
-template<typename TActFn, typename TVal = float>
+template<typename TActFn, typename TVal = value_t>
 class Neuron
 {
+    using CynapseType = Cynapse<TActFn, TVal>;
+
     TVal value;
-    std::vector<Cynapse*> input;
-    std::vector<Cynapse*> output;
     TActFn activation_fn;
+    
+    std::vector<CynapseType*> inputs;
+    std::vector<CynapseType*> outputs;
+};
+
+template<typename TActFn, typename TVal>
+class Cynapse
+{
+    using NeuronType = Neuron<TActFn, TVal>;
+
+    TVal weight;
+    NeuronType* input;
+    NeuronType* output;
+
 };
